1f25d3e2de4c76a9e7407f2f3104b920d9bbf717
Mohammad Ferdiyanzah <mohammad.ferdiyanzah@dexagroup.com>
2024-11-06 14:15:53 +0700

    change: adjust apk path

--- mobile/src/test/java/utils/BaseTest.java
+++ mobile/src/test/java/utils/BaseTest.java
@@ -16,36 +16,20 @@ public class BaseTest {
   protected static AndroidDriver driver;
   protected static WebDriverWait wait;
 
+  private static final String APPIUM_HOST = System.getProperty("appium.host",
+      System.getenv("APPIUM_HOST") != null ? System.getenv("APPIUM_HOST") : "localhost");
+  private static final String APPIUM_URL = String.format("http://%s:4723", APPIUM_HOST);
+
   @AndroidFindBy(xpath = "//android.widget.TextView[@content-desc=\"store itemtext\"]")
   private List<WebElement> productNames;
 
   @AndroidFindBy(xpath = "//android.widget.Button[contains(@resource-id,'addToCartBtn')]")
   private WebElement addToCartButton;
 
-  private static void resetUiAutomator2() {
-    try {
-      String[] commands = {
-          "adb shell pm clear io.appium.uiautomator2.server",
-          "adb shell pm clear io.appium.uiautomator2.server.test",
-          "adb shell am force-stop io.appium.uiautomator2.server",
-          "adb shell am force-stop io.appium.uiautomator2.server.test"
-      };
-
-      for (String command : commands) {
-        Process process = Runtime.getRuntime().exec(command);
-        process.waitFor();
-      }
-      Thread.sleep(2000); // Give some time for the services to reset
-    } catch (Exception e) {
-      System.out.println("Warning: Failed to reset UiAutomator2: " +
-          e.getMessage());
-    }
-  }
-
   public static void setUp() throws MalformedURLException {
 
     UiAutomator2Options options = new UiAutomator2Options()
-        .setApp("D:\\Course\\QA\\milestone-project-qa-2\\mobile\\Android-MyDemoAppRN.1.3.0.build-244.apk")
+        .setApp("mobile\\Android-MyDemoAppRN.1.3.0.build-244.apk")
         .setAutomationName("UiAutomator2")
         .setAppPackage("com.saucelabs.mydemoapp.rn")
         .setAppActivity(".MainActivity")
@@ -54,7 +38,7 @@ public class BaseTest {
         .setNewCommandTimeout(Duration.ofSeconds(60));
 
     try {
-      driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+      driver = new AndroidDriver(new URL(APPIUM_URL), options);
       wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
       // Wait for app to be fully loaded
@@ -68,8 +52,7 @@ public class BaseTest {
 
     } catch (Exception e) {
       System.out.println("Failed to initialize driver: " + e.getMessage());
-      resetUiAutomator2(); // Try resetting UiAutomator2 if initialization fails
-      driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options); //
+      driver = new AndroidDriver(new URL(APPIUM_URL), options); //
     }
 
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
